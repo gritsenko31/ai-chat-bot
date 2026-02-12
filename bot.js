a347cbe3a6735dab4bb587ab03298e0b85327c07
Dmytro Hrytsenko <113560146+gritsenko31@users.noreply.github.com>
2026-02-12 04:41:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ require('dotenv').config();  // ПЕРВЫЙ!
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ 
+  model: "gemini-1.5-flash-001" 
+});
 
 
 const { Telegraf, Markup } = require('telegraf');
